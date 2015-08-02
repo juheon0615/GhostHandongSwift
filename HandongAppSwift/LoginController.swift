6d17ccd7d8e7a821982b30a638b044f2a589dfd6
ghost <juheon0615@gmail.com>
2015-08-02 16:15:28 +0900

    juheon

--- HandongAppSwift/LoginController.swift
+++ HandongAppSwift/LoginController.swift
@@ -11,6 +11,35 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    var ID = "ghost"
+    var PW = "ghost"
+    
+    @IBOutlet weak var idText: UITextField!
+    
+    @IBOutlet weak var pwText: UITextField!
+    
+    @IBAction func LoginExe(sender: AnyObject) {
+        
+        if (idText.text == ID && idText.text == PW){
+            print("log-in successful")
+            self.performSegueWithIdentifier("SegueToCalender", sender: self)
+        }
+        else {
+//            let alert = UIAlertView()
+//            alert.title = "Login failed"
+//            alert.message = "wrong input!"
+//            alert.addButtonWithTitle("Go back to main page")
+// error pop-up
+            print("log-in failed")
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+    }
+    
+    @IBAction func DismissLogIn(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,6 +53,8 @@ class LoginController: UIViewController {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
+    
+    
     
     
     
